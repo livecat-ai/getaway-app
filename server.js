9e9a509d6de2b7603e31162032a77d2237f9bcbf
Livecate <johnlockwood@me.com>
2021-08-29 22:30:40 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,160 @@
+// import dotenv to access local .env variables
+require('dotenv').config();
+
+// Import fetch
+const fetch = require('node-fetch');
+
+// Run the express server
+const express = require('express');
+const app = express();
+// Assign the server port
+const port = 3000;
+
+// // Start up bodyParser
+// const bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Start up Cors
+const cors = require('cors');
+app.use(cors());
+
+// // Initialize the main project folder
+app.use(express.static('website'));
+
+// Spin up the server
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+
+// app.get('/', function (req, res) {
+//     console.log('in root');
+//     res.send("hello world");
+// });
+
+let appData = [];
+
+app.post('/weather', function (req, res) {
+    const tripDate = new Date(req.body.tripDate);
+    const info = {};
+
+    info.place = req.body.place;
+    info.daysToTrip = daysToTrip(tripDate);
+    info.tripDate = tripDate.getTime();
+
+    console.log(`requesting gps coords for: ${info.place}`);
+    getGpsFromPlaceName(info.place)
+    .then(data => {
+        info.lat = data.geonames[0].lat;
+        info.lon = data.geonames[0].lng;
+        info.country = data.geonames[0].countryName;
+        // console.log(info);
+        return getForcastWeather(info.lat, info.lon)
+    })
+    .then(data => {
+        info.temperature = data.data[0].temp;
+        info.description = data.data[0].weather.description;
+        // appData.push(info);
+        console.log(info);
+        res.send(info);
+    })
+});
+
+app.post('/image', function (req, res) {
+    const place = req.body.place;
+    getImageFromPlaceName(place)
+    .then(data => {
+        console.log(data);
+        res.send(data);
+    })
+})
+
+const getData = async(url) => {
+    const res = await fetch(url);
+    try {
+        const data = await res.json();
+        return data;
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+}
+
+function gpsFromPlacenameUrl(placeName) {
+    // Use Geonames to get gps coords for a given a
+    // place name and a country code
+    const username = process.env.GEONAMES_USER;
+    const baseUrl = "http://api.geonames.org/searchJSON?";
+    const placeUrl = "q=" + placeName;
+    const userUrl = "&username=" + username;
+    return baseUrl + placeUrl + userUrl;
+}
+
+function getGpsFromPlaceName(placeName) {
+    const url = gpsFromPlacenameUrl(placeName);
+    return getData(url);
+}
+
+function forcastWeatherUrl(lat, lon) {
+    const api_key = process.env.WEATHERBIT_KEY;
+    const baseUrl = "https://api.weatherbit.io/v2.0/current";
+    return `${baseUrl}?lat=${lat}&lon=${lon}&key=${api_key}`;
+}
+
+function getForcastWeather(lat, lon) {
+    const url = forcastWeatherUrl(lat, lon);
+    return getData(url);
+}
+
+function imageFromPlaceNameUrl(placeName) {
+    const apiKey = process.env.PIXABAY_KEY;
+    const baseUrl = "https://pixabay.com/api/";
+    return `${baseUrl}?key=${apiKey}&q=${placeName}&image_type=photo`;
+}
+
+function getImageFromPlaceName(placeName) {
+    const url = imageFromPlaceNameUrl(placeName);
+    return getData(url);
+}
+
+const getAverageWeather = async (lat, lon, startDate, endDate) => {
+    const api_key = process.env.WEATHERBIT_KEY;
+    const baseUrl = "https://api.weatherbit.io/v2.0/normals";
+    const searchUrl = `${baseUrl}?lat=${lat}&lon=${lon}`
+                     + `&start_day=${startDate}`
+                     + `&end_day=${endDate}&tp=monthly&key=${api_key}`;
+    console.log(`fetching: ${searchUrl}`);
+    const res = await fetch(searchUrl);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+}
+
+function dateToWeatherbitFormat(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${date.getMonth()}-${date.getDate()}`;
+}
+
+function daysToTrip(tripDate) {
+    const now = new Date(Date.now());
+    const days = dateDeltaInDays(now, tripDate);
+    console.log(days);
+    return days;
+}
+
+function dateDeltaInDays(startDate, endDate) {
+    const delta = endDate.getTime() - startDate.getTime();
+    const minutes = Math.floor(delta / 60000);
+    const hours = Math.round(minutes / 60);
+    const days = Math.round(hours / 24);
+    return days
+};
---
+++ website/app.js
@@ -0,0 +1,99 @@
+// const { default: fetch } = require("node-fetch");
+
+function updateUI(data) {
+    const weather = document.getElementById("weather");
+    weather.innerHTML = data;
+}
+
+function updateImage(data) {
+    const imageId = document.getElementById("image");
+    imageId.src = data;
+}
+
+const postData = async ( url = '', data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData;
+    }
+    catch(error){
+        console.log("error", error);
+    }
+}
+
+// function dateDelta(startDate, endDate) {
+//     const delta = endDate.getTime() - startDate.getTime();
+//     const minutes = Math.floor(delta / 60000);
+//     const hours = Math.round(minutes / 60);
+//     const days = Math.round(hours / 24);
+//     return days
+// };
+
+// function dateToWeatherbitFormat(date) {
+//     const month = date.getMonth() + 1;
+//     const day = date.getDate();
+//     return `${date.getMonth()}-${date.getDate()}`;
+// }
+
+
+function handleSubmit() {
+    console.log("Submit button pressed");
+    const place = document.getElementById("place").value;
+    // const startDate = new Date(document.getElementById("start").value);
+    // const endDate = new Date(document.getElementById("end").value);
+    // postData('/weather', {'place': place, 'tripDate': startDate})
+    // .then(weather => {
+    //     console.log(weather);
+    //     updateUI(JSON.stringify(weather));
+    // });
+    postData('/image', {'place': place})
+    .then(data => {
+        imageUrl = data.hits[0].largeImageURL;
+        // console.log(imageUlr);
+        console.log(imageUrl);
+        updateImage(imageUrl);
+    });
+};
+
+//     postData('/gps', {'place': place})
+//     // const body = {"place":place, 
+//     //               "start":dateToWeatherbitFormat(startDate),
+//     //               "end":dateToWeatherbitFormat(endDate)
+//     // };
+//     // const body = {"place":place, 
+//     //               "start":startDate,
+//     //               "end":endDate };
+//     // // const body = {"place":place};
+//     // postData('/add', body)                 
+//     .then(data => {
+//         const details = data.geonames[0];
+//         const country = details.countryName;
+//         const lat = details.lat;
+//         const lon = details.lng;
+//         console.log(data.geonames[0]);
+//         postData('/weather', {'lat':lat, 'lon':lon})
+//         .then(data => {
+//             console.log(data);
+//             const temp = data.data;
+//             // updateUI(temp);
+//             updateUI(JSON.stringify(data.data));
+//         })
+//         // updateUI(JSON.stringify(data.geonames[0]));
+//     });
+// }
+
+
+
+
+
+// postData('/add', {answer:42});
